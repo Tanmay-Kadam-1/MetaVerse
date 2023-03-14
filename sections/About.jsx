17017690b230eec9c936ca76d9a2d32fbe8e4766
Tanmay Kadam <justcoding1482@gmail.com>
2023-03-14 21:41:22 +0530

    About Section Changes

--- sections/About.jsx
+++ sections/About.jsx
@@ -36,6 +36,12 @@ const About = () => (
 				<span className="font-extrabold text-white">explore</span> the madness
 				of the Metaverse by scrolling down
 			</motion.p>
+			<motion.img
+				variants={fadeIn("up", "tween", 0.3, 1)}
+				src="/arrow-down.svg"
+				alt="arrow down"
+				className="w-[18px] h-[28px] object-contain mt-[28px]"
+			/>
 		</motion.div>
 	</section>
 );
