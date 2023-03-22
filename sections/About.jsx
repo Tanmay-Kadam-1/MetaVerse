cbeb32ce5bbfe15ebd7d0c377bc848fcafb94bc6
Tanmay Kadam <justcoding1482@gmail.com>
2023-03-22 22:19:36 +0530

    Explore Card Completed

--- sections/About.jsx
+++ sections/About.jsx
@@ -34,7 +34,7 @@ const About = () => (
 				easily explore the metaverse world you want, turn your dreams into
 				reality. Let's{" "}
 				<span className="font-extrabold text-white">explore</span> the madness
-				of the Metaverse by scrolling down
+				of the Metaverse by scrolling down.
 			</motion.p>
 			<motion.img
 				variants={fadeIn("up", "tween", 0.3, 1)}
