a07d595dfc095268864d7e0d019a130288a7d6c5
Tanmay Kadam <justcoding1482@gmail.com>
2023-03-24 23:10:23 +0530

    Get Started Completed

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -2,7 +2,16 @@
 
 const GetStarted = () => (
 	<section className={`${styles.paddings} relative z-10`}>
-		Get Started Section
+		<motion.div
+			variants={planetVariants("left")}
+			className={`flex-1 ${styles.flexCenter}`}
+		>
+			<img
+				src="/get-started.png"
+				alt="get-started"
+				className="w-[90%] h-[90%] object-contain"
+			/>
+		</motion.div>{" "}
 	</section>
 );
 
