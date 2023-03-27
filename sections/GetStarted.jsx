c8f49b7f8142367f46e20076870f21850cf2a7bc
Tanmay Kadam <justcoding1482@gmail.com>
2023-03-27 23:53:01 +0530

    Get Started Changes

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -12,7 +12,7 @@ const GetStarted = () => (
 				className="w-[90%] h-[90%] object-contain"
 			/>
 			<motion.div
-				variants={fadeIn("left", "tween", 0.2, 1)}
+				variants={fadeIn("left", "", 0.2, 1)}
 				className="flex-[0.75] ex justiy-center flex-col"
 			></motion.div>
 		</motion.div>{" "}
