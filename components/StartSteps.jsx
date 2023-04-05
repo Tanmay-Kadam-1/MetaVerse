4ff0da8cf88e48022179c3231098827b7fc28e37
Tanmay Kadam <justcoding1482@gmail.com>
2023-04-05 22:55:04 +0530

    Start Section Changes

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,7 +1,9 @@
 const StartSteps = () => (
-  <div>
-    start steps
-  </div>
+	<div className={`${styles.flexCenter} flex-row`}>
+		<div
+			className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]`}
+		></div>
+	</div>
 );
 
 export default StartSteps;
