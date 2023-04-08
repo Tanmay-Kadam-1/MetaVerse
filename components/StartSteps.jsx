48fcd0b7c89089b3505937a29988608da54b622d
Tanmay Kadam <justcoding1482@gmail.com>
2023-04-08 23:11:50 +0530

    StartSteps changes

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -3,6 +3,7 @@ const StartSteps = () => (
 		<div
 			className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]`}
 		></div>
+		<p className="font-bold text-[20px] text-white">{number}</p>
 	</div>
 );
 
