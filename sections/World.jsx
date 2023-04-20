16aa18b7332b38493e4d14604ba814a4e64fe082
Tanmay Kadam <justcoding1482@gmail.com>
2023-04-20 22:55:46 +0530

    1 Motion div added

--- sections/World.jsx
+++ sections/World.jsx
@@ -2,6 +2,22 @@
 
 import { motion } from "framer-motion";
 
-const World = () => <section>World secti</section>;
+const World = () => <section>    <motion.div
+variants={staggerContainer}
+initial="hidden"
+whileInView="show"
+viewport={{ once: false, amount: 0.25 }}
+className={`${styles.innerWidth} mx-auto flex flex-col`}
+>
+
+<TypingText title="| People on the World" textStyles="text-center" />
+<TitleText
+  title={(
+    <>Track friends around you and invite them to play together in the same
+      world
+    </>
+  )}
+  textStyles="text-center"
+/></section>;
 
 export default World;
