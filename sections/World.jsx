eeeadfc7baa1df2618f64b7a3dbf7780eb49ae72
Tanmay Kadam <justcoding1482@gmail.com>
2023-04-26 23:38:50 +0530

    Changes

--- sections/World.jsx
+++ sections/World.jsx
@@ -30,15 +30,9 @@ const World = () => (
 				variants={fadeIn("up", "tween", 0.3, 1)}
 				className="relative mt-[68px] flex w-full h-[550px]"
 			>
-				<img src="/map.png" alt="map" className="w-full h-full object-cover" />
+				<div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]"></div>
 
-				<div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
-					<img src="people-01.png" alt="people" className="w-full h-full" />
-				</div>
-
-				<div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
-					<img src="/people-02.png" alt="people" className="w-full h-full" />
-				</div>
+				<div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]"></div>
 
 				<div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]"></div>
 			</motion.div>
