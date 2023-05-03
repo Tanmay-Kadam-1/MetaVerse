6eb03d37555a4786620ed9a0cd6272370559b0cc
Tanmay Kadam <justcoding1482@gmail.com>
2023-05-03 19:44:51 +0530

    Changes

--- sections/World.jsx
+++ sections/World.jsx
@@ -34,7 +34,7 @@ const World = () => (
 
 				<div className=" top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]"></div>
 
-				<div className=" top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]"></div>
+				<div className="  left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]"></div>
 			</motion.div>
 		</motion.div>
 	</section>
